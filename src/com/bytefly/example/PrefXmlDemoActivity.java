04238b2527b0d4a75bf43ae8e175f0aa54c917d7
dheerm <dheerm@gmail.com>
2014-02-10 17:59:28 -0500

    initial commit

---
+++ src/com/bytefly/example/PrefXmlDemoActivity.java
@@ -0,0 +1,77 @@
+package com.bytefly.example;
+
+import edu.pace.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class PrefXmlDemoActivity extends Activity {
+	private Button prefBtn;
+	private Preferences frag;
+    /** Called when the activity is first created. */
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+            frag=new Preferences();
+            prefBtn = (Button) findViewById(R.id.prefBtn);
+            prefBtn.setOnClickListener(new OnClickListener() {
+
+                    public void onClick(View v) {
+                            getFragmentManager().beginTransaction().add(android.R.id.content,frag,"PREFS").commit();
+                            v.setVisibility(View.INVISIBLE);
+                    }
+            });
+    }
+	boolean CheckboxPreference;
+	String ListPreference;
+	String editTextPreference;
+	String ringtonePreference;
+	String secondEditTextPreference;
+	String customPref;
+
+	private void getPrefs() {
+		// Get the xml/preferences.xml preferences
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		CheckboxPreference = prefs.getBoolean("checkboxPref", true);
+		ListPreference = prefs.getString("listPref", "nr1");
+		editTextPreference = prefs.getString("editTextPref",
+				"Nothing has been entered");
+		ringtonePreference = prefs.getString("ringtonePref",
+				"DEFAULT_RINGTONE_URI");
+		secondEditTextPreference = prefs.getString("SecondEditTextPref",
+				"Nothing has been entered");
+		// Get the custom preference
+		SharedPreferences mySharedPreferences = getSharedPreferences(
+				"myCustomSharedPrefs", Activity.MODE_PRIVATE);
+		customPref = mySharedPreferences.getString("myCusomPref", "");
+		
+		Toast.makeText(getBaseContext(),
+				"CheckboxPreference "+CheckboxPreference,
+				Toast.LENGTH_LONG).show();
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		getPrefs();
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if(frag!=null && frag.isAdded()){
+			prefBtn.setVisibility(View.VISIBLE);
+			getFragmentManager().beginTransaction().remove(frag).commit();
+		}else{
+			super.onBackPressed();
+		}
+		
+	}
+}
---
+++ src/com/bytefly/example/Preferences.java
@@ -0,0 +1,43 @@
+package com.bytefly.example;
+
+import edu.pace.R;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.widget.Toast;
+
+public class Preferences extends PreferenceFragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.prefs);
+		// Get the custom preference
+		Preference customPref = (Preference) findPreference("customPref");
+		customPref
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+					public boolean onPreferenceClick(Preference preference) {
+						Toast.makeText(getActivity(),
+								"The custom preference has been clicked",
+								Toast.LENGTH_LONG).show();
+						SharedPreferences customSharedPreference = getActivity().getSharedPreferences(
+								"myCustomSharedPrefs", Activity.MODE_PRIVATE);
+						SharedPreferences.Editor editor = customSharedPreference
+								.edit();
+						editor.putString("myCustomPref",
+								"The preference has been clicked");
+						editor.commit();
+						return true;
+					}
+
+				});
+		
+	}
+
+
+}
